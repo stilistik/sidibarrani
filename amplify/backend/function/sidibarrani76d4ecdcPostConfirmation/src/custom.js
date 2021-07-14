4596e6191058d5a03da1e714ec1403a6819588ea
Philipp Gerber <philipp@retinai.com>
2021-07-14 11:09:19 +0200

    feat: synch cognito user with ddb

---
+++ amplify/backend/function/sidibarrani76d4ecdcPostConfirmation/src/custom.js
@@ -0,0 +1,31 @@
+const aws = require("aws-sdk");
+const ddb = new aws.DynamoDB();
+
+exports.handler = async (event, context, callback) => {
+  console.log("Running lambda", event);
+  try {
+    if (event.request.userAttributes.sub) {
+      const date = new Date();
+
+      const params = {
+        Item: {
+          __typename: { S: "User" },
+          id: { S: event.request.userAttributes.sub },
+          username: { S: event.userName },
+          email: { S: event.request.userAttributes.email },
+          createdAt: { S: date.toISOString() },
+          updatedAt: { S: date.toISOString() },
+        },
+        TableName: process.env.USERTABLE,
+      };
+
+      await ddb.putItem(params).promise();
+      console.log("Successfully synchronized Cognito user with DynamoDB");
+    } else {
+      console.log("Nothing was added to DynamoDB");
+    }
+  } catch (err) {
+  } finally {
+    callback(null, event);
+  }
+};
