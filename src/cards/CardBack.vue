254f1463cf268f64d10df6f40bc034e4e3a2c305
Philipp Gerber <philipp@retinai.com>
2021-07-25 22:42:36 +0200

    feat: add cardback

---
+++ src/cards/CardBack.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="p-2 bg-white" :style="style" src="assets/card_back_2.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardBack",
+});
+</script>
