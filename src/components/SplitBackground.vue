81d8998850751a8069330a203944f864d47fcfb3
Philipp Gerber <philipp@retinai.com>
2021-12-21 21:22:14 +0100

    fix: use safe access, add transition

--- src/components/SplitBackground.vue
+++ src/components/SplitBackground.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="z-0 absolute w-full h-full flex">
     <div
-      class="w-full h-full absolute top-0 left-0"
+      class="w-full h-full absolute top-0 left-0 transition-all"
       :class="[bg1class]"
       :style="{ clipPath: 'polygon(0 0, 54% 0, 44% 100%, 0% 100%)' }"
     />
     <div
-      class="w-full h-full absolute top-0 left-0"
+      class="w-full h-full absolute top-0 left-0 transition-all"
       :class="[bg2class]"
       :style="{ clipPath: 'polygon(56% 0, 100% 0, 100% 100%, 46% 100%)' }"
     />
@@ -45,14 +45,14 @@ export default defineComponent({
   setup(props) {
     const bg1class = computed(() => {
       return props.hovered
-        ? colorClasses[props.color1 as Color].bgHover
-        : colorClasses[props.color1 as Color].bg;
+        ? colorClasses[props.color1 as Color]?.bgHover
+        : colorClasses[props.color1 as Color]?.bg;
     });
 
     const bg2class = computed(() => {
       return props.hovered
-        ? colorClasses[props.color2 as Color].bgHover
-        : colorClasses[props.color2 as Color].bg;
+        ? colorClasses[props.color2 as Color]?.bgHover
+        : colorClasses[props.color2 as Color]?.bg;
     });
 
     return reactive({
